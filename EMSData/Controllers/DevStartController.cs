ce380f5856b71d763c787ff46ea80d7928f6f963
MarkWoods <markw@criticalcommunication.ca>
2015-11-14 15:59:59 -0700

    Still Playing

---
+++ EMSData/Controllers/DevStartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EMSData.Controllers
+{
+    public class DevStartController : Controller
+    {
+        // GET: DevStart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DevStart/Welcome
+
+        public String Welcome(string name, int ID = 1)
+        {
+                //return "This is the Welcome action method...";
+                return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + ID);
+        }
+    
+    }
+
+}
---
+++ EMSData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EMSData.Startup))]
+namespace EMSData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
